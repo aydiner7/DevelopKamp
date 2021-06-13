2c7cc99e81212c9ddb0730b5528fcb52177cdf70
hasanaydiner <hasanaydiner7@hotmail.com>
2021-06-14 01:38:36 +0300

    Join

--- LinqProject/Program.cs
+++ LinqProject/Program.cs
@@ -22,19 +22,87 @@ namespace LinqProject
                 new Product{ProductId=4, CategoryId=2, ProductName="IPhone 6", QuantityPerUnit="2 GB RAM", UnitPrice=4000, UnitsInStock=0},
                 new Product{ProductId=5, CategoryId=2, ProductName="SAMSUNG S10", QuantityPerUnit="4 GB RAM", UnitPrice=3000, UnitsInStock=11}
             };
+            //WhereTest(products);
+            //AnyTest(products);
+            //FindTest(products);
+            //FindAllTest(products);
+            //AscDescTest(products);
 
+            JoinTest(categories, products);
 
-            var result = products.Where(p => p.UnitPrice > 4000 && p.UnitsInStock>4);
+        }
+
+        private static void JoinTest(List<Category> categories, List<Product> products)
+        {
+            var result = from p in products
+                         join c in categories
+                         on p.CategoryId equals c.CategoryId
+                         where p.UnitPrice > 5000
+                         orderby p.UnitPrice descending
+                         select new ProductDto { ProductId = p.ProductId, CategoryName = c.CategoryName, ProductName = p.ProductName, UnitePrice = p.UnitPrice };
+            foreach (var item in result)
+            {
+                Console.WriteLine("{0} -- {1} -- {2}", item.ProductName, item.CategoryName, item.UnitePrice);
+            }
+        }
+
+        private static void AscDescTest(List<Product> products)
+        {
+            //Single Line Query
+            // Senaryolar Arttırılabilir.
+            var result = products.Where(p => p.ProductName.Contains("top")).OrderByDescending(p => p.UnitPrice).ThenByDescending(p => p.ProductName);
             foreach (var item in result)
             {
                 Console.WriteLine(item.ProductName);
             }
         }
-          
 
+        private static void FindAllTest(List<Product> products)
+        {
+            //FindAll Koşula uyan tüm nesneleri getirir
+            //Where ile aynı mantıkta çalışır.
+            var result = products.FindAll(p => p.ProductName.Contains("top"));
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.ProductName);
+            }
+        }
 
-            
-        
+        private static void FindTest(List<Product> products)
+        {
+            //Find (Lambda) koşula uygun nesneye getirir. Nesneye ait tüm bilgilere erişebilirsin.
+            // Genellikle detay bilgileri için kullanılır.
+            var result = products.Find(p => p.ProductId == 3);
+            Console.WriteLine(result.UnitPrice);
+        }
+
+        private static void AnyTest(List<Product> products)
+        {
+            // Any Liste içerisinde bu eleman var mı yok mu kontrolü.
+            var result = products.Any(p => p.ProductName == "HP LaptoP");
+            Console.WriteLine(result);
+        }
+
+        private static void WhereTest(List<Product> products)
+        {
+            // Where Liste içerisindeki koşullarımıza uygun elemanları getirir.
+            var result = products.Where(p => p.UnitPrice > 4000 && p.UnitsInStock > 4);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.ProductName);
+            }
+        }
+
+        class ProductDto
+        {
+            public int ProductId { get; set; }
+
+            public string CategoryName { get; set; }
+
+            public string ProductName { get; set; }
+
+            public decimal UnitePrice { get; set; }
+        }
 
         class Product
         {
